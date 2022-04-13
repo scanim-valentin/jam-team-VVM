8dfcbb38a3be80d333baa16f2984c4ea474dcd22
scanim-valentin <valentin.guerbette@hotmail.fr>
2022-04-13 19:58:59 +0200

    Projectile + Health

---
+++ Marre des canards 2D/Assets/Scripts/DuckScripts/HealthScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour
+{
+
+    //Default = ??
+    public int HealthValue ; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(HealthValue == 0){
+            //Call death event
+            Destroy(gameObject) ; 
+        }
+    }
+}
---
+++ Marre des canards 2D/Assets/Scripts/DuckScripts/ThrowProjectile.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowProjectile : MonoBehaviour
+{
+    //Projectile used by ducks
+    public GameObject projectile ; 
+
+    //Throwing force of ducks
+    public float throwForce ; 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //To be changed later using unity input manager
+        if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    GameObject projectileInstance = Instantiate(projectile, transform.localPosition+ new Vector3(2f,0f,0f), transform.rotation);
+                    projectileInstance.GetComponent<Rigidbody2D>().AddForce(throwForce*transform.right, ForceMode2D.Impulse);      
+                }
+    }
+}
