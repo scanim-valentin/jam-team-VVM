3a8e03bd6ee49f140bcebf5049129ff3a5847002
Matthias <75242607+moutth@users.noreply.github.com>
2022-04-15 22:59:40 +0200

    baguette

--- Marre des canards 2D/Assets/Scripts/HUD/PainDisplay.cs
+++ Marre des canards 2D/Assets/Scripts/HUD/PainDisplay.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PainDisplay : MonoBehaviour
 {
+    public int nbPainsAManger = 5;
+    public Vector3 lastMiePosition;
+    private int painManges = 0;
+
+    private Vector3 increaseBarrePain;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        increaseBarrePain = (lastMiePosition - transform.position)/nbPainsAManger;
     }
 
     // Update is called once per frame
-    void Update()
+    void IncreaseBarre()
     {
-        
+        painManges++;
+        if (painManges > nbPainsAManger)
+        {
+
+        } else
+        {
+            transform.position += increaseBarrePain;
+        }
     }
 }
